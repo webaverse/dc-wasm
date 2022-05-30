271a8297ea1f565a644d10dd4a3580020f55a2da
Avaer Kazmer <a@modules.io>
2022-05-29 23:27:55 -0400

    Initial commit

---
+++ compile.sh
@@ -0,0 +1,17 @@
+# emsdk 2.0.6
+mkdir -p bin
+echo 'building main...'
+# m = 64*1024; s = 350000000; Math.floor(s/m)*m;
+# emcc -s WASM=1 -s NO_EXIT_RUNTIME=1 -s TOTAL_MEMORY=419430400 -s ALLOW_MEMORY_GROWTH=1 -O3
+emcc -s WASM=1 -s NO_EXIT_RUNTIME=1 -s TOTAL_MEMORY=52428800 -D__linux__ -s ALLOW_MEMORY_GROWTH=0 -O3 \
+  objectize.cc \
+  FastNoise.cpp \
+  DualContouring/main.cc DualContouring/octree.cc DualContouring/vectorMath.cc DualContouring/qef.cc DualContouring/svd.cc DualContouring/density.cc DualContouring/chunkDamageBuffer.cc DualContouring/mesh.cc \
+  -I. \
+  -o bin/dc.js
+  sed -Ei 's/dc.wasm/bin\/dc.wasm/g' bin/dc.js
+  echo 'let accept, reject;const p = new Promise((a, r) => {  accept = a;  reject = r;});Module.postRun = () => {  accept();};Module.waitForLoad = () => p;run();export default Module;' >> bin/dc.js
+echo done
+
+# Prevent compile window auto close after error, to see the error details. https://askubuntu.com/a/20353/1012283
+# exec $SHELL
---
+++ objectize.cc
@@ -0,0 +1,36 @@
+#include <emscripten.h>
+#include "DualContouring/main.h"
+#include <deque>
+#include <map>
+
+extern "C" {
+
+EMSCRIPTEN_KEEPALIVE void setChunkSize(int newChunkSize) {
+    return DualContouring::setChunkSize(newChunkSize);
+}
+
+EMSCRIPTEN_KEEPALIVE void generateChunkDataDualContouring(float x, float y, float z) {
+    return DualContouring::generateChunkData(x, y, z);
+}
+
+EMSCRIPTEN_KEEPALIVE void setChunkLodDualContouring(float x, float y, float z, int lod) {
+    return DualContouring::setChunkLod(x, y, z, lod);
+}
+
+EMSCRIPTEN_KEEPALIVE void clearTemporaryChunkDataDualContouring() {
+    return DualContouring::clearTemporaryChunkData();
+}
+
+EMSCRIPTEN_KEEPALIVE void clearChunkRootDualContouring(float x, float y, float z) {
+    return DualContouring::clearChunkRoot(x, y, z);
+}
+
+EMSCRIPTEN_KEEPALIVE uint8_t *createChunkMeshDualContouring(float x, float y, float z) {
+    return DualContouring::createChunkMesh(x, y, z);
+}
+
+EMSCRIPTEN_KEEPALIVE bool drawDamage(float x, float y, float z, float radius, float value, float *outPositions, unsigned int *outPositionsCount) {
+    return DualContouring::drawDamage(x, y, z, radius, value, outPositions, outPositionsCount);
+}
+
+} // extern "C"
