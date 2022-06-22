534597132e320c221d0deb98c57665f1d752b21c
Avaer Kazmer <a@modules.io>
2022-06-21 23:35:58 -0400

    Add vertex water buffer

--- DualContouring/mesh.cc
+++ DualContouring/mesh.cc
@@ -55,3 +55,49 @@ uint8_t *VertexBuffer::getBuffer() const {
 
   return buffer;
 }
+
+uint8_t *VertexWaterBuffer::getBuffer() const {
+  // calculate size
+  size_t neededSize = 
+    sizeof(uint32_t) +
+    positions.size() * sizeof(positions[0]) +
+
+    sizeof(uint32_t) +
+    normals.size() * sizeof(normals[0]) +
+
+    sizeof(uint32_t) +
+    biomes.size() * sizeof(biomes[0]) +
+
+    sizeof(uint32_t) +
+    indices.size() * sizeof(indices[0]);
+
+  // allocate buffer
+  uint8_t *buffer = (uint8_t *)malloc(neededSize);
+  int index = 0;
+
+  // positions
+  *((uint32_t *)(buffer + index)) = positions.size();
+  index += sizeof(uint32_t);
+  std::memcpy(buffer + index, &positions[0], positions.size() * sizeof(positions[0]));
+  index += positions.size() * sizeof(positions[0]);
+
+  // normals
+  *((uint32_t *)(buffer + index)) = normals.size();
+  index += sizeof(uint32_t);
+  std::memcpy(buffer + index, &normals[0], normals.size() * sizeof(normals[0]));
+  index += normals.size() * sizeof(normals[0]);
+
+  // biomes
+  *((uint32_t *)(buffer + index)) = biomes.size();
+  index += sizeof(uint32_t);
+  std::memcpy(buffer + index, &biomes[0], biomes.size() * sizeof(biomes[0]));
+  index += biomes.size() * sizeof(biomes[0]);
+
+  // indices
+  *((uint32_t *)(buffer + index)) = indices.size();
+  index += sizeof(uint32_t);
+  std::memcpy(buffer + index, &indices[0], indices.size() * sizeof(indices[0]));
+  index += indices.size() * sizeof(indices[0]);
+
+  return buffer;
+}
--- DualContouring/mesh.h
+++ DualContouring/mesh.h
@@ -9,16 +9,27 @@
 
 typedef std::vector<vm::vec3> PositionBuffer;
 typedef std::vector<vm::vec3> NormalBuffer;
-typedef std::vector<vm::ivec4> BiomeBuffer;
-typedef std::vector<vm::vec4> BiomeWeightBuffer;
+typedef std::vector<vm::ivec4> BiomesBuffer;
+typedef std::vector<vm::vec4> BiomesWeightBuffer;
 typedef std::vector<int> IndexBuffer;
+typedef std::vector<int> BiomeBuffer;
 
 class VertexBuffer {
 public:
     PositionBuffer positions;
     NormalBuffer normals;
+    BiomesBuffer biomes;
+    BiomesWeightBuffer biomesWeights;
+    IndexBuffer indices;
+
+    uint8_t *getBuffer() const;
+};
+
+class VertexWaterBuffer {
+public:
+    PositionBuffer positions;
+    NormalBuffer normals;
     BiomeBuffer biomes;
-    BiomeWeightBuffer biomesWeights;
     IndexBuffer indices;
 
     uint8_t *getBuffer() const;
