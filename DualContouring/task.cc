bfed0a822800835eb88ed1711df174e7a729e5d7
Avaer Kazmer <a@modules.io>
2022-06-24 02:24:27 -0400

    Add task code

---
+++ DualContouring/task.cc
@@ -0,0 +1,61 @@
+#include "task.h"
+#include "instance.h"
+#include <iostream>
+
+//
+
+Task::Task(DCInstance *inst, std::vector<vm::ivec3> &&chunkPositions, int lod, int flags, std::function<void *()> &&fn) :
+  inst(inst),
+  chunkPositions(chunkPositions),
+  lod(lod),
+  flags(flags),
+  fn(std::move(fn))
+  {}
+Task::~Task() {}
+
+bool Task::canLock() const {
+  return inst->canLock(chunkPositions, lod, flags);
+}
+void Task::lock() {
+  inst->lock(chunkPositions, lod, flags);
+}
+void Task::unlock() {
+  inst->unlock(chunkPositions, lod, flags);
+}
+void *Task::run() {
+  return fn();
+}
+
+//
+
+TaskQueue::TaskQueue(DCInstance *inst) :
+  inst(inst)
+  {}
+TaskQueue::~TaskQueue() {}
+
+void TaskQueue::pushTask(Task *task) {
+  std::unique_lock<std::mutex> lock(taskLock);
+  tasks.push_back(task);
+  taskCondVar.notify_all();
+}
+Task *TaskQueue::popLockTask() {
+  std::unique_lock<std::mutex> lock(taskLock);
+
+  Task *task = nullptr;
+  taskCondVar.wait(lock, [&]() -> bool {
+    for (int i = 0; i < tasks.size(); i++) {
+      if (tasks[i]->canLock()) {
+        task = tasks[i];
+        tasks.erase(tasks.begin() + i);
+        return true;
+      }
+    }
+    return false;
+  });
+  if (task == nullptr) {
+    std::cout << "failed to pop task!" << std::endl;
+    abort();
+  }
+  task->lock();
+  return task;
+}
---
+++ DualContouring/task.h
@@ -0,0 +1,49 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include "vectorMath.h"
+#include <vector>
+#include <mutex>
+#include <semaphore>
+
+//
+
+class DCInstance;
+
+//
+
+class Task {
+public:
+    DCInstance *inst;
+    std::vector<vm::ivec3> chunkPositions;
+    int lod;
+    int flags;
+    std::function<void *()> fn;
+
+    Task(DCInstance *inst, std::vector<vm::ivec3> &&chunkPositions, int lod, int flags, std::function<void *()> &&fn);
+    ~Task();
+
+    bool canLock() const;
+    void lock();
+    void unlock();
+    void *run();
+    std::pair<bool, void *> tryLockRun();
+};
+
+//
+
+class TaskQueue {
+public:
+    DCInstance *inst;
+    std::vector<Task *> tasks;
+    std::mutex taskLock;
+    std::condition_variable taskCondVar;
+
+    TaskQueue(DCInstance *inst);
+    ~TaskQueue();
+    
+    void pushTask(Task *task);
+    Task *popLockTask();
+};
+
+#endif // TASK_H
