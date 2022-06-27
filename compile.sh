5a0080d3995d085f524fcb9b6a25c6165b708a8f
Avaer Kazmer <a@modules.io>
2022-06-27 11:02:58 -0400

    Abstract NUM_THREADS in compile.sh

--- compile.sh
+++ compile.sh
@@ -2,7 +2,8 @@ mkdir -p bin
 echo 'building main...'
 # m = 64*1024; s = 1000000000; Math.floor(s/m)*m;
 # emcc -s NO_EXIT_RUNTIME=1 -s TOTAL_MEMORY=52428800 -D__linux__ -s ALLOW_MEMORY_GROWTH=0 -sWASM_WORKERS=1 -g -s ASSERTIONS=1 -fexceptions \
-emcc -D NUM_THREADS=10 -sNO_EXIT_RUNTIME=1 -s TOTAL_MEMORY=1999896576 -pthread -sPTHREAD_POOL_SIZE=10 -sPTHREAD_POOL_SIZE_STRICT=10 -s ALLOW_MEMORY_GROWTH=0 -O3 \
+NUM_THREADS=10
+emcc -D NUM_THREADS=$NUM_THREADS -sNO_EXIT_RUNTIME=1 -s TOTAL_MEMORY=1999896576 -pthread -sPTHREAD_POOL_SIZE=$NUM_THREADS -sPTHREAD_POOL_SIZE_STRICT=$NUM_THREADS -s ALLOW_MEMORY_GROWTH=0 -O3 \
   objectize.cc \
   FastNoise.cpp util.cc vector.cc worley.cc \
   DualContouring/main.cc DualContouring/noise.cc DualContouring/vectorMath.cc DualContouring/qef.cc DualContouring/svd.cc DualContouring/biomes.cc DualContouring/density.cc DualContouring/mesh.cc DualContouring/octree.cc DualContouring/chunk.cc DualContouring/instance.cc DualContouring/context.cc DualContouring/task.cc DualContouring/result.cc DualContouring/lock.cc DualContouring/sync.cc \
