8530163a6d6f51e761346ec6e2360f18303aa3c3
Avaer Kazmer <a@modules.io>
2022-06-27 03:28:25 -0400

    Increase memory ordering restrictions in sync primitives

--- DualContouring/sync.cc
+++ DualContouring/sync.cc
@@ -8,7 +8,7 @@ Mutex::Mutex() : flag(false) {}
 Mutex::~Mutex() {}
 void Mutex::lock() {
   for (;;) {
-    bool oldValue = flag.test_and_set(std::memory_order_acquire);
+    bool oldValue = flag.test_and_set(std::memory_order_seq_cst);
     if (!oldValue) {
       break;
     } else {
@@ -17,11 +17,11 @@ void Mutex::lock() {
   }
 }
 void Mutex::unlock() {
-  flag.clear(std::memory_order_release);
+  flag.clear(std::memory_order_seq_cst);
   flag.notify_one();
 }
 bool Mutex::try_lock() {
-  bool oldValue = flag.test_and_set(std::memory_order_acquire);
+  bool oldValue = flag.test_and_set(std::memory_order_seq_cst);
   return !oldValue;
 }
 
@@ -32,20 +32,20 @@ Semaphore::Semaphore() : value(0) {}
 Semaphore::~Semaphore() {}
 void Semaphore::wait() {
   for (;;) {
-    int newValue;
+    int oldValue;
     {
       std::unique_lock<Mutex> lock(mutex);
       /* EM_ASM({
         console.log('test value', $0);
       }, (int)value); */
-      newValue = --value;
-      if (newValue >= 0) {
+      oldValue = value.fetch_sub(1, std::memory_order_seq_cst);
+      if (oldValue > 0) {
         break;
       } else {
-        newValue = ++value;
+        value.fetch_add(1, std::memory_order_seq_cst);
       }
     }
-    value.wait(newValue);
+    value.wait(oldValue);
   }
 }
 void Semaphore::signal() {
