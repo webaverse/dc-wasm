2587b1ce65f3c07a9d8813a9997f3a42c01cd409
Avaer Kazmer <a@modules.io>
2022-06-29 17:05:24 -0400

    Hash cleanup

--- DualContouring/cache.cc
+++ DualContouring/cache.cc
@@ -2,7 +2,9 @@
 
 uint64_t getIndex(int x, int y) {
     uint64_t result = uint16_t(x);
+    result = (result << 16) | uint16_t(0);
     result = (result << 16) | uint16_t(y);
+    result = (result << 16) | uint16_t(0);
     return result;
 }
 uint64_t getIndex(int x, int y, int z) {
