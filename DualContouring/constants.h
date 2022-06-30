419f8d5c913a969967288f2e9dfdd2f40cda736a
Avaer Kazmer <a@modules.io>
2022-06-30 07:58:37 -0400

    Add constants.h

---
+++ DualContouring/constants.h
@@ -0,0 +1,10 @@
+#ifndef _CONSTANTS_H_
+#define _CONSTANTS_H_
+
+constexpr int numThreads = NUM_THREADS;
+constexpr uint32_t stackSize = 1024 * 1024;
+constexpr int chunkSize = 16;
+constexpr int CHUNK_CACHE_RANGE = 16;
+constexpr int cacheWidth = chunkSize * CHUNK_CACHE_RANGE;
+
+#endif // _CONSTANTS_H_
