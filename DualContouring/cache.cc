a6dfb19bd589fd6dab34165a44a3736ad388bce1
Avaer Kazmer <a@modules.io>
2022-07-01 15:54:41 -0400

    World hash function debugging

--- DualContouring/cache.cc
+++ DualContouring/cache.cc
@@ -3,15 +3,31 @@
 
 //
 
-constexpr int hashSeed = 0;
+// constexpr int hashSeed = 0;
 
 uint32_t getCacheIndexWorld(int x, int y) {
-    std::array<int, 2> coords = {x, y};
-    return XXH32(coords.data(), coords.size(), hashSeed);
+    uint32_t x2 = (((uint32_t)x >> 16) ^ (uint32_t)x) * 0x45d9f3b;
+    x2 = ((x2 >> 16) ^ x2) * 0x45d9f3b;
+    x2 = (x2 >> 16) ^ x2;
+
+    uint32_t y2 = (((uint32_t)y >> 16) ^ (uint32_t)y) * 0x119de1f3;
+    y2 = ((y2 >> 16) ^ y2) * 0x119de1f3;
+    y2 = (y2 >> 16) ^ y2;
+
+    return x2 ^ y2;
 }
 uint32_t getCacheIndexWorld(int x, int y, int z) {
-    std::array<int, 3> coords = {x, y, z};
-    return XXH32(coords.data(), coords.size(), hashSeed);
+    uint32_t x2 = (((uint32_t)x >> 16) ^ (uint32_t)x) * 0x45d9f3b;
+    x2 = ((x2 >> 16) ^ x2) * 0x45d9f3b;
+    x2 = (x2 >> 16) ^ x2;
+
+    uint32_t y2 = (uint32_t)y * 2654435761;
+
+    uint32_t z2 = (((uint32_t)z >> 16) ^ (uint32_t)z) * 0x119de1f3;
+    z2 = ((z2 >> 16) ^ z2) * 0x119de1f3;
+    z2 = (z2 >> 16) ^ z2;
+
+    return x2 ^ y2 ^ z2;
 }
 
 //
