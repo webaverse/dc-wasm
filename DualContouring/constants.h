67c4cae2d451eaba332b04a181a5f9cd931af38d
Avaer Kazmer <a@modules.io>
2022-07-01 11:26:22 -0400

    Dead constant cleanup

--- DualContouring/constants.h
+++ DualContouring/constants.h
@@ -2,7 +2,7 @@
 #define _CONSTANTS_H_
 
 constexpr int numThreads = NUM_THREADS;
-constexpr uint32_t stackSize = 1024 * 1024;
+// constexpr uint32_t stackSize = 1024 * 1024;
 constexpr int chunkSize = 16;
 constexpr int CHUNK_CACHE_RANGE = 16;
 constexpr int cacheWidth = chunkSize * CHUNK_CACHE_RANGE;
