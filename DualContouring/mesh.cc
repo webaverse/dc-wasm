539faeb460ce2ec9ac5b6f192339d53b55d98a57
Avaer Kazmer <a@modules.io>
2022-07-03 10:53:59 -0400

    Export biomes uvs in vertex buffer

--- DualContouring/mesh.cc
+++ DualContouring/mesh.cc
@@ -4,18 +4,22 @@
 uint8_t *TerrainVertexBuffer::getBuffer() const {
   // calculate size
   size_t neededSize = 
+    // positions
     sizeof(uint32_t) +
     positions.size() * sizeof(positions[0]) +
-
+    // normals
     sizeof(uint32_t) +
     normals.size() * sizeof(normals[0]) +
-
+    // biomes
     sizeof(uint32_t) +
     biomes.size() * sizeof(biomes[0]) +
-
+    // biomesWeights
     sizeof(uint32_t) +
     biomesWeights.size() * sizeof(biomesWeights[0]) +
-
+    // biomesUvs
+    sizeof(uint32_t) +
+    biomesUvs.size() * sizeof(biomesUvs[0]) +
+    // indices
     sizeof(uint32_t) +
     indices.size() * sizeof(indices[0]);
 
@@ -47,6 +51,12 @@ uint8_t *TerrainVertexBuffer::getBuffer() const {
   std::memcpy(buffer + index, &biomesWeights[0], biomesWeights.size() * sizeof(biomesWeights[0]));
   index += biomesWeights.size() * sizeof(biomesWeights[0]);
 
+  // biomesUvs
+  *((uint32_t *)(buffer + index)) = biomesUvs.size();
+  index += sizeof(uint32_t);
+  std::memcpy(buffer + index, &biomesUvs[0], biomesUvs.size() * sizeof(biomesUvs[0]));
+  index += biomesUvs.size() * sizeof(biomesUvs[0]);
+
   // indices
   *((uint32_t *)(buffer + index)) = indices.size();
   index += sizeof(uint32_t);
@@ -60,22 +70,24 @@ void TerrainVertexBuffer::pushVertexData(const VertexData &vertexData) {
   normals.push_back(vertexData.normal);
   biomes.push_back(vertexData.biomes);
   biomesWeights.push_back(vertexData.biomesWeights);
+  biomesUvs.push_back(vertexData.biomeUvs);
 }
 
 //
 
 uint8_t *LiquidVertexBuffer::getBuffer() const {
   // calculate size
-  size_t neededSize = 
+  size_t neededSize =
+    // positions
     sizeof(uint32_t) +
     positions.size() * sizeof(positions[0]) +
-
+    // normals
     sizeof(uint32_t) +
     normals.size() * sizeof(normals[0]) +
-
+    // biomes
     sizeof(uint32_t) +
     biomes.size() * sizeof(biomes[0]) +
-
+    // indices
     sizeof(uint32_t) +
     indices.size() * sizeof(indices[0]);
 
