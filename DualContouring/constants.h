e21218afdc16808663f7666aa93102f7cab7a2c5
Avaer Kazmer <a@modules.io>
2022-07-18 00:51:54 -0400

    Add frustumCullDistancePenalty constant

--- DualContouring/constants.h
+++ DualContouring/constants.h
@@ -2,9 +2,10 @@
 #define _CONSTANTS_H_
 
 constexpr int numThreads = NUM_THREADS;
-// constexpr uint32_t stackSize = 1024 * 1024;
 constexpr int chunkSize = 16;
 constexpr int CHUNK_CACHE_RANGE = 8;
 constexpr int cacheWidth = chunkSize * CHUNK_CACHE_RANGE;
+
+constexpr float frustumCullDistancePenalty = 10000.f;
 
 #endif // _CONSTANTS_H_
