b53d0db14fbad57f18dbbfe95cd0964cfff324c4
Avaer Kazmer <a@modules.io>
2022-07-22 08:21:11 -0400

    Update constants

--- DualContouring/constants.h
+++ DualContouring/constants.h
@@ -6,7 +6,8 @@ constexpr int chunkSize = 16;
 constexpr int CHUNK_CACHE_RANGE = 8;
 constexpr int cacheWidth = chunkSize * CHUNK_CACHE_RANGE;
 
-constexpr float frustumCullDistancePenalty = 10000.f;
+constexpr float frustumCullDistancePenalty = 1000.f;
+constexpr float priorityDistancePenalty = 10000.f;
 
 enum class PEEK_FACES : int {
   FRONT = 0,
