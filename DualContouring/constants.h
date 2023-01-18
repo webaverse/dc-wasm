88577488f30af6f2ba45393d6576c0eed12b2a89
Avaer Kazmer <a@modules.io>
2023-01-18 07:44:16 -0500

    Remove old constants

--- DualContouring/constants.h
+++ DualContouring/constants.h
@@ -1,22 +1,22 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
-constexpr int numThreads = NUM_THREADS;
-constexpr int chunkSize = 16;
-constexpr int CHUNK_CACHE_RANGE = 8;
-constexpr int cacheWidth = chunkSize * CHUNK_CACHE_RANGE;
+// constexpr int numThreads = NUM_THREADS;
+// constexpr int chunkSize = 16;
+// constexpr int CHUNK_CACHE_RANGE = 8;
+// constexpr int cacheWidth = chunkSize * CHUNK_CACHE_RANGE;
 
-constexpr float frustumCullDistancePenalty = 1000.f;
-constexpr float priorityDistancePenalty = 10000.f;
+// constexpr float frustumCullDistancePenalty = 1000.f;
+// constexpr float priorityDistancePenalty = 10000.f;
 
-enum class PEEK_FACES : int {
-  FRONT = 0,
-  BACK,
-  LEFT,
-  RIGHT,
-  TOP,
-  BOTTOM,
-  NONE
-};
+// enum class PEEK_FACES : int {
+//   FRONT = 0,
+//   BACK,
+//   LEFT,
+//   RIGHT,
+//   TOP,
+//   BOTTOM,
+//   NONE
+// };
 
 #endif // _CONSTANTS_H_
