c4b4c2843a39ac1d167fd130bc3d694e35b8ae00
Avaer Kazmer <a@modules.io>
2023-01-18 07:56:41 -0500

    Update compile script

--- compile.sh
+++ compile.sh
@@ -1,23 +1,23 @@
 mkdir -p bin
 echo 'building main...'
-# m = 64*1024; s = 512 * 1024 * 1024; Math.floor(s/m)*m;
+# m = 64*1024; s = 200 * 1024 * 1024; Math.floor(s/m)*m;
 # emcc -s NO_EXIT_RUNTIME=1 -s TOTAL_MEMORY=52428800 -D__linux__ -s ALLOW_MEMORY_GROWTH=0 -sWASM_WORKERS=1 -g -s ASSERTIONS=1 -fexceptions \
+
+# -O3
 # -g -s ASSERTIONS=1 -fsanitize=address
-NUM_THREADS=8
-emcc -D NUM_THREADS=$NUM_THREADS -sNO_EXIT_RUNTIME=1 -s TOTAL_MEMORY=536870912 -pthread -sPTHREAD_POOL_SIZE=$NUM_THREADS -sPTHREAD_POOL_SIZE_STRICT=$NUM_THREADS -s ALLOW_MEMORY_GROWTH=0 -O3 \
+
+emcc -sNO_EXIT_RUNTIME=1 -s TOTAL_MEMORY=314572800 -s ALLOW_MEMORY_GROWTH=0 \
+  -O3 \
   objectize.cc \
-  FastNoise.cpp util.cc vector.cc worley.cc xxhash.c \
+  util.cc vector.cc MC.cc VHACD.cc \
   DualContouring/main.cc DualContouring/vectorMath.cc DualContouring/qef.cc DualContouring/svd.cc DualContouring/density.cc DualContouring/mesh.cc DualContouring/octree.cc DualContouring/instance.cc DualContouring/context.cc \
   -I. \
   -o bin/dc.js
 
-  #sed -Ei 's/var Module=typeof Module!="undefined"\?Module:\{\};/var Module = globalThis.Module??{};/g' bin/dc.js
-  # sed -Ei 's/var asm=createWasm\(\);/asmLibraryArg.__cxa_atexit=()=>{};var asm=createWasm();/g' bin/dc.js
-  sed -Ei 's/importScripts\(e.data.urlOrBlob\)/e.data.urlOrBlob="\/dc.js";importScripts(e.data.urlOrBlob)/g' bin/dc.worker.js
+  # sed -Ei 's/importScripts\(e.data.urlOrBlob\)/importScripts(e.data.urlOrBlob.replace(\/dc-worker\\.js.*$\/, "dc.js"))/g' bin/dc.worker.js
   echo 'let accept, reject;const p = new Promise((a, r) => {accept = a;  reject = r;});Module.postRun = () => {accept();};Module.waitForLoad = () => p;' >> bin/dc.js
+  sed -Ei 's/scriptDirectory\+path/"\/"+path/g' bin/dc.js
   cp bin/dc.js bin/dc.module.js
   echo 'export default Module;' >>bin/dc.module.js
-echo done
 
-# Prevent compile window auto close after error, to see the error details. https://askubuntu.com/a/20353/1012283
-# exec $SHELL
+echo done
